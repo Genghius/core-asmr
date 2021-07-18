5d16068efaaaeb1b615dbb0453c70a49204197c8
Genghius <genghius.khanate03@gmail.com>
2021-07-18 14:07:44 +0100

    added swapon & swapoff

---
+++ src/swapon.asm
@@ -0,0 +1,28 @@
+section .text
+        global _start
+
+_start:
+        pop rax
+        cmp rax, 1
+        jle _errend
+        pop rdi         ;discard argv[0]
+        pop rdi         ;argv[1]
+
+        mov rax, 167    ;sys_swapon
+        xor rsi, rsi    ;no one actually uses these flags right?
+        syscall
+
+        cmp rax, 0      ;check for errors
+        jne _errend
+
+        jmp _end
+
+_errend:
+        mov rax, 60
+        mov rdi, 1
+        syscall
+
+_end:
+        mov rax, 60
+        xor rdi, rdi
+        syscall
